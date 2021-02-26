eca8411d80bb1680cd86ae99ca90d18b7d3fe368
Ravindra Singh <ravindrarajpoot9628172@gmail.com>
2021-02-26 07:46:10 +0530

    promiss block_BJaajw

--- block-BJaajw/code/index.js
+++ block-BJaajw/code/index.js
@@ -3,15 +3,9 @@ let input = document.querySelector('input');
 function changeHandle(event) {
   if (event.keyCode === 13) {
     let val = event.target.value;
-    let getData = async () => {
-      let serarchData = await fetch(
-        `https://api.domainsdb.info/v1/domains/search?domain=${val}`
-      );
-      serarchData
-        .then((res) => console.log(res))
-        .catch((err) => console.log(err));
-    };
-    getData();
+    fetch(
+      `https://api.domainsdb.info/v1/domains/search?domain=${val}`
+    ).then((data) => data.json());
   }
 }
 
